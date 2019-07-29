68d5dd2f3c281f6d22ba1948a2e342305b19badc
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-07-29 09:14:30 +0200

    add numerValue

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 'use strict';
-const number= document.querySelector(('.field__fill-number'));
-
-console.log('number.value');
-
-function fillNumber(event) {
-    const numberadd= event.currentTarget.value;
-    console.log(numberadd)
-}
-
-number.addEventListener('keyup', fillNumber);
+const number= document.querySelector('.field__fill-number');
+const btn= document.querySelector('.btn');
 
 
 //generar un número aleatorio con la ayuda de Math.random y Math.ceil, y mostrarlo por la consola
-
-// Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-  }
-  
-  // Anda, chacho, píntame en la consola un número random hasta 100;
-  const myRandomNumber = getRandomNumber(100);
-  
-  console.log(`Mi número aleatorio es ${myRandomNumber}`);
+}
+
+
+function clickbtn (){
+
+// Anda, chacho, píntame en la consola un número random hasta 100;
+const myRandomNumber = getRandomNumber(100);
+console.log(`Mi número aleatorio es ${myRandomNumber}`);
+
+// al pulsar el botón de "prueba", acceder al contenido del input y mostrarlo en la consola
+const numberValue= parseInt(number.value);
+console.log(numberValue);
+
+//comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS GANADO, CAMPEONA!")
+
+
+}
+
+  btn.addEventListener('click',clickbtn);
