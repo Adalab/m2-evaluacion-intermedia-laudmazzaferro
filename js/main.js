0e1e9330f42e0b808a49821f09ab93b5c8cb3d24
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-08-06 08:07:06 +0200

    add buttton

--- js/main.js
+++ js/main.js
@@ -1,42 +1,54 @@
 'use strict';
-const number = document.querySelector('.field__fill-number');
+let number = document.querySelector('.field__fill-number');
 const btn = document.querySelector('.btn');
 const text = document.querySelector('.text');
 const lost = document.querySelector('.lost');
+const resetBtn =document.querySelector('.reset-button');
 let cont = 0;
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-const myRandomNumber = getRandomNumber(100);
+let myRandomNumber = getRandomNumber(100);
 //console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
 function clickbtn() {
   const numberValue = parseInt(number.value);
   //console.log(number.value);
+  cont = cont + 1;
+  lost.innerHTML = cont;
 
   if (myRandomNumber === numberValue) {
     text.innerHTML = '¡HAS GANADO, CAMPEONA!';
-    //console.log(cont);
-    cont = cont + 1;
+    console.log('ganador');
   } else if (number.value === ''){
-    //console.log('holaaa');
+    console.log('vacio');
     text.innerHTML = 'Campo vacio, escribir un numero';
   }else if (numberValue>100 | numberValue< 0){
     text.innerHTML = 'El numero tiene que estar entre 0 y 100,Te has salido del rango';
-    cont = cont + 1;
+    console.log('raango');
   }else if (myRandomNumber > numberValue) {
     text.innerHTML = 'Demasiado bajo';
-    cont = cont + 1;
-    lost.innerHTML = cont;
-    //console.log(cont);
+    console.log('bajo');
   } else {
     text.innerHTML = 'Demasiado alto';
-    cont = cont + 1;
-    lost.innerHTML = cont;
-    //console.log(cont);
+    console.log('alto');
   }
+
 }
 
+function reset(){
+  cont=0;
+  number.value='';
+  text.innerHTML = 'Escribe un numero y Prueba';
+  lost.innerHTML = cont;
+  myRandomNumber = getRandomNumber(100);
+  console.log(myRandomNumber );
+}
+
+console.log(myRandomNumber );
+
 btn.addEventListener('click', clickbtn);
+number.addEventListener('change',clickbtn);
+resetBtn.addEventListener('click',reset);
