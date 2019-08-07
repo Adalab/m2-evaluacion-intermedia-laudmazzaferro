9e6d1c15ffc147100a9d198bef873228797ebab1
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-08-07 18:35:54 +0200

    remove console.log

--- js/main.js
+++ js/main.js
@@ -9,52 +9,38 @@ let cont = 0;
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-
 let myRandomNumber = getRandomNumber(100);
-//console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
 function clickbtn() {
   const numberValue = parseInt(number.value);
-  //console.log(number.value);
+ 
   cont = cont + 1;
   lost.innerHTML = cont +' Intentos';
-  console.log(numberValue + 'ganador');
-
   if (myRandomNumber === numberValue) {
     text.innerHTML = 'HAS GANADO, CON TAN SOLO';
-    console.log('ganador');
   } else if (number.value === ''){
-    console.log('vacio');
     text.innerHTML = 'Campo vacío, escribir un número';
   }else if (numberValue>100 | numberValue< 0){
     text.innerHTML = 'El número tiene que estar entre 0 y 100,Te has salido del rango';
-    console.log('raango');
   }else if (myRandomNumber > numberValue) {
     text.innerHTML = 'Demasiado bajo';
-    console.log('bajo');
   } else {
     text.innerHTML = 'Demasiado alto';
-    console.log('alto');
   }
-
 }
-
 function enter(event){
   if (event.keyCode === 13){
     clickbtn();
   }
 }
-
 function reset(){
   cont=0;
   number.value='';
   text.innerHTML = 'Escribe un numero y Prueba';
   lost.innerHTML = cont;
   myRandomNumber = getRandomNumber(100);
-  console.log(myRandomNumber );
 }
 
-console.log(myRandomNumber );
 
 btn.addEventListener('click', clickbtn);
 number.addEventListener('keyup',enter);
